5a20d3c510d2181a3bd40e735bca524afd33d6e5
Thomas Theux <t.grikschas@gmail.com>
2019-01-17 12:29:07 +0100

    Started project

---
+++ Prototyping Basics/Assets/Scripts/CharacterSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSpawn : MonoBehaviour {
+
+	public GameObject playerGO;
+	public Vector3 defaultSpawnPosition = new Vector3(0, 1, 0);
+	public float playerSpawnDistance = 4.0f;
+
+
+	// Spawn new players with the amount set in GameVariables
+	private void Start() {
+		for (int i = 0; i < GameVariables.playerCount; i++) {
+			GameObject newPlayer = Instantiate(playerGO);
+			newPlayer.name = "Player" + i;
+			newPlayer.transform.position = defaultSpawnPosition + new Vector3(playerSpawnDistance * i, 0, 0);
+		}
+	}
+
+}
---
+++ Prototyping Basics/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public int playerCount = 0;
+
+
+	private void Awake() {
+		GameVariables.playerCount = playerCount;
+	}
+
+}
